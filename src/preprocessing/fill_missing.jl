4a0fd60abe8121c50cf5eb07d8f63df056ace167
Daniel Hopp <daniel.hopp@metr.systems>
2020-02-13 13:39:00 +0100

    Adds fill_missing_scalar + tests

--- src/preprocessing/fill_missing.jl
+++ src/preprocessing/fill_missing.jl
@@ -1,10 +1,11 @@
 include("scaler.jl")
 using DataFrames
 
-export fill_missing
+export fill_missing_aggregate
+export fill_missing_scalar
 
 """
-_given a dataframe and optionally columns, return the dataframe with missing values filled in with columns mean, median, etc._
+_given a dataframe and optionally columns, return the dataframe with missing values filled in with column's mean, median, etc._
 #### parameters:
     data : DataFrame
         dataframe to min max scale
@@ -15,13 +16,39 @@ _given a dataframe and optionally columns, return the dataframe with missing val
 #### returns: DataFrame
     a new dataframe with desired columns altered
 """
-function fill_missing(data::DataFrame, columns::Array=[]; method)
+function fill_missing_aggregate(data::DataFrame, columns::Array=[]; method)
     tmp = copy(data)
     numeric_cols = numeric_columns(tmp)
     iter_columns = isempty(columns) ? numeric_cols :
                    intersect(numeric_cols, Symbol.(columns))
     for col in iter_columns
         tmp[!, Symbol(col)] = recode(tmp[!, Symbol(col)], missing => method(skipmissing(tmp[!, Symbol(col)])))
+    end
+    return tmp
+end
+
+"""
+_given a dataframe and optionally columns, return the dataframe with missing values filled in with provided scalar_
+#### parameters:
+    data : DataFrame
+        dataframe to min max scale
+    columns : Array{Symbol}
+        an array of column names to scale, default is all numeric
+    scalar : Any
+        any desired value to replace missings, must be of type of column(s)
+#### returns: DataFrame
+    a new dataframe with desired columns altered
+"""
+function fill_missing_scalar(data::DataFrame, columns::Array=[]; scalar::Any)
+    tmp = copy(data)
+    iter_columns = isempty(columns) ? names(tmp) :
+                   intersect(names(tmp), Symbol.(columns))
+    for col in iter_columns
+        try
+            tmp[!, Symbol(col)] = recode(tmp[!, Symbol(col)], missing => scalar)
+        catch e
+            throw(ArgumentError("Scalar must match variable type of ALL columns."))
+        end
     end
     return tmp
 end
--- test/preprocessing/test_fill_missing.jl
+++ test/preprocessing/test_fill_missing.jl
@@ -5,29 +5,43 @@ test_data = DataFrame(
     w = ["one", "two", "three", "four"],
     x = [1, 2, 3, missing],
     y = [15, missing, 90, 15],
-    z = [-1, -20, 20, -20],
+    z = [-1, -20, 20, -20]
 )
 
 @testset "fill_missing" begin
     # mean, all columns
-    @test isequal(fill_missing(test_data, method=mean), DataFrame(
+    @test isequal(fill_missing_aggregate(test_data, method=mean), DataFrame(
         w = ["one", "two", "three", "four"],
         x = [1.0, 2.0, 3.0, 2.0],
         y = [15.0, 40.0, 90.0, 15.0],
         z = [-1.0, -20.0, 20.0, -20.0]
     ))
     # mode, all columns
-    @test isequal(fill_missing(test_data, method=median), DataFrame(
+    @test isequal(fill_missing_aggregate(test_data, method=median), DataFrame(
         w = ["one", "two", "three", "four"],
         x = [1.0, 2.0, 3.0, 2.0],
         y = [15.0, 15.0, 90.0, 15.0],
         z = [-1.0, -20.0, 20.0, -20.0]
     ))
     # mean, subset of columns
-    @test isequal(fill_missing(test_data, [:y, :z],method=mean), DataFrame(
+    @test isequal(fill_missing_aggregate(test_data, [:y, :z],method=mean), DataFrame(
         w = ["one", "two", "three", "four"],
         x = [1, 2, 3, missing],
         y = [15.0, 40.0, 90.0, 15.0],
         z = [-1.0, -20.0, 20.0, -20.0]
     ))
+
+    # scalar, correct type
+    @test isequal(fill_missing_scalar(test_data, [:x, :y], scalar=100), DataFrame(
+        w = ["one", "two", "three", "four"],
+        x = [1, 2, 3, 100],
+        y = [15, 100, 90, 15],
+        z = [-1, -20, 20, -20]
+    ))
+    # scalar, incorrect type
+    @test try
+            fill_missing_scalar(test_data, [:x, :y], scalar="string")
+        catch ex
+            ex
+        end == ArgumentError("Scalar must match variable type of ALL columns.")
 end
